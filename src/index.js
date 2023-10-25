5ba4299f34f8a029ba2080964cb65d873bb8a3fd
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2023-10-25 23:05:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 //Write your code here
+class API{
+  constructor(url, method= 'GET'){
+    this.url=url;
+    this.method=method;
+    this.#secure=url.startsWith("https")
+  }
+  isSecure(){
+    return this.#secure
+  }
+  updateURL(url){
+    this.url=url
+    this.#secure=url.startsWith("https")
+  }
+}
 
 
 
